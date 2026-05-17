e740ce10fe6d654b2460274d1e67b7fa81c3f455
Chris <125362103+Chriss0309@users.noreply.github.com>
2026-05-17 00:52:11 -0400

    Bulk sync: 27 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newString = ''
+        for character in s:
+            if character.isalnum():
+                newString += character.lower()
+            
+        return newString == newString[::-1]
